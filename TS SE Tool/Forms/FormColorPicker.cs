bdafbcd0adcbea414619e1dd7aa7144437fd0620
Gaev Kirill <liptoh890@gmail.com>
2020-04-28 18:47:26 +0500

    License header

--- TS SE Tool/Forms/FormColorPicker.cs
+++ TS SE Tool/Forms/FormColorPicker.cs
@@ -1,23 +1,18 @@
-﻿// PsColorPicker.cs
-// 
-// Copyright (c) 2007-2010, OpenPainter.org, and based on the work of
-//               2005 Danny Blanchard (scrabcakes@gmail.com)
-// 
-// The contents of this file are subject to the Mozilla Public License
-// Version 1.1 (the "License"); you may not use this file except in
-// compliance with the License. You may obtain a copy of the License at
-// 
-// http://www.mozilla.org/MPL/
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code is OpenPainter.
-// 
-// The Initial Developer of the Original Code is OpenPainter.org.
-// All Rights Reserved.
+﻿/*
+   Copyright 2016-2020 LIPtoH <liptoh.codebase@gmail.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 //---------------------------------------------------------------------------
 // Project:           Adobe Color Picker Clone 1
