2b8ff063512578d1f1dbaccb8a3f19f7609def9c
Gaev Kirill <liptoh890@gmail.com>
2022-01-20 19:09:41 +0500

    Save Delivery log structure [add]

---
+++ TS SE Tool/CustomClasses/Save/Items/Delivery_log.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_SE_Tool.Save.Items
+{
+    class Delivery_log
+    {
+        internal int version { get; set; } = 0;
+
+        internal List<string> entries { get; set; } = new List<string>();
+
+        internal int cached_jobs_count { get; set; } = 0;
+
+        internal Delivery_log()
+        { }
+
+        internal Delivery_log(string[] _input)
+        {
+            string tagLine = "", dataLine = "";
+
+            foreach (string currentLine in _input)
+            {
+                if (currentLine.Contains(':'))
+                {
+                    string[] splittedLine = currentLine.Split(new char[] { ':' }, 2);
+
+                    tagLine = splittedLine[0].Trim();
+                    dataLine = splittedLine[1].Trim();
+                }
+                else
+                {
+                    tagLine = currentLine.Trim();
+                    dataLine = "";
+                }
+
+                switch (tagLine)
+                {
+                    case "":
+                        {
+                            break;
+                        }
+
+                    case "version":
+                        {
+                            version = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case "entries":
+                        {
+                            entries.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("entries["):
+                        {
+                            entries.Add(dataLine);
+                            break;
+                        }
+
+                    case "cached_jobs_count":
+                        {
+                            cached_jobs_count = int.Parse(dataLine);
+                            break;
+                        }
+
+                }
+            }
+        }
+
+        internal string PrintOut(uint _version, string _nameless)
+        {
+            string returnString = "";
+
+            StringBuilder returnSB = new StringBuilder();
+
+            returnSB.AppendLine("delivery_log : " + _nameless + " {");
+
+            returnSB.AppendLine(" version: " + version.ToString());
+
+            returnSB.AppendLine(" entries: " + entries.Count);
+            for (int i = 0; i < entries.Count; i++)
+                returnSB.AppendLine(" entries[" + i + "]: " + entries[i]);
+
+            returnSB.AppendLine(" cached_jobs_count: " + cached_jobs_count.ToString());
+
+            returnSB.AppendLine("}");
+
+            returnString = returnSB.ToString();
+
+            return returnString;
+        }
+    }
+}
---
+++ TS SE Tool/CustomClasses/Save/Items/Delivery_log_Entry.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_SE_Tool.Save.Items
+{
+    class Delivery_log_Entry
+    {
+        List<string> Params = new List<string>();
+
+        internal Delivery_log_Entry()
+        { }
+
+        internal Delivery_log_Entry(string[] _input)
+        {
+            string tagLine = "", dataLine = "";
+
+            foreach (string currentLine in _input)
+            {
+                if (currentLine.Contains(':'))
+                {
+                    string[] splittedLine = currentLine.Split(new char[] { ':' }, 2);
+
+                    tagLine = splittedLine[0].Trim();
+                    dataLine = splittedLine[1].Trim();
+                }
+                else
+                {
+                    tagLine = currentLine.Trim();
+                    dataLine = "";
+                }
+
+                switch (tagLine)
+                {
+                    case "":
+                        {
+                            break;
+                        }
+
+                    case "params":
+                        {
+                            Params.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("params["):
+                        {
+                            Params.Add(dataLine);
+                            break;
+                        }
+
+                }
+            }
+        }
+
+        internal string PrintOut(uint _version, string _nameless)
+        {
+            string returnString = "";
+
+            StringBuilder returnSB = new StringBuilder();
+
+            returnSB.AppendLine("delivery_log_entry : " + _nameless + " {");
+
+            returnSB.AppendLine(" params: " + Params.Count);
+            for (int i = 0; i < Params.Count; i++)            
+                returnSB.AppendLine(" params[" + i + "]: " + Params[i]); 
+
+            returnSB.AppendLine("}");
+
+            returnString = returnSB.ToString();
+
+            return returnString;
+        }
+    }
+}
