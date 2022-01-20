805dcf96ad8f1c75258a51d3b1036b6b1eceb7cd
Gaev Kirill <liptoh890@gmail.com>
2022-01-20 14:47:13 +0500

    Save Economy event Queue structure [add]

---
+++ TS SE Tool/CustomClasses/Save/Items/Economy_event.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_SE_Tool.Save.Items
+{
+    class Economy_event
+    {
+        internal uint time { get; set; } = 0;
+
+        internal string unit_link { get; set; } = "";
+
+        internal int param { get; set; } = 0;
+
+        internal Economy_event()
+        { }
+
+        internal Economy_event(string[] _input)
+        {
+            string tagLine = "", dataLine = "";
+
+            foreach (string currentLine in _input)
+            {
+                if (currentLine.Contains(':'))
+                {
+                    string[] splittedLine = currentLine.Split(new char[] { ':' }, 2);
+
+                    tagLine = splittedLine[0].Trim();
+                    dataLine = splittedLine[1].Trim();
+                }
+                else
+                {
+                    tagLine = currentLine.Trim();
+                    dataLine = "";
+                }
+
+                switch (tagLine)
+                {
+                    case "":
+                        {
+                            break;
+                        }
+
+                    case "time":
+                        {
+                            time = uint.Parse(dataLine);
+                            break;
+                        }
+
+                    case "unit_link":
+                        {
+                            unit_link = dataLine;
+                            break;
+                        }
+
+                    case "param":
+                        {
+                            param = int.Parse(dataLine);
+                            break;
+                        }
+
+                }
+            }
+        }
+
+        internal string PrintOut(uint _version, string _nameless)
+        {
+            string returnString = "";
+
+            StringBuilder returnSB = new StringBuilder();
+
+            returnSB.AppendLine("economy_event : " + _nameless + " {");
+
+            returnSB.AppendLine(" time: " + time.ToString());
+
+            returnSB.AppendLine(" unit_link: " + unit_link);
+
+            returnSB.AppendLine(" param: " + param.ToString());
+
+            returnSB.AppendLine("}");
+
+            returnString = returnSB.ToString();
+
+            return returnString;
+        }
+    }
+}
---
+++ TS SE Tool/CustomClasses/Save/Items/Economy_event_Queue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_SE_Tool.Save.Items
+{
+    class Economy_event_Queue
+    {
+        internal List<string> data { get; set; } = new List<string>();
+
+        internal Economy_event_Queue()
+        { }
+
+        internal Economy_event_Queue(string[] _input)
+        {
+            string tagLine = "", dataLine = "";
+
+            foreach (string currentLine in _input)
+            {
+                if (currentLine.Contains(':'))
+                {
+                    string[] splittedLine = currentLine.Split(new char[] { ':' }, 2);
+
+                    tagLine = splittedLine[0].Trim();
+                    dataLine = splittedLine[1].Trim();
+                }
+                else
+                {
+                    tagLine = currentLine.Trim();
+                    dataLine = "";
+                }
+
+                switch (tagLine)
+                {
+                    case "":
+                        {
+                            break;
+                        }
+
+                    case "data":
+                        {
+                            data.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("data["):
+                        {
+                            data.Add(dataLine);
+                            break;
+                        }
+                }
+            }
+        }
+
+        internal string PrintOut(uint _version, string _nameless)
+        {
+            string returnString = "";
+
+            StringBuilder returnSB = new StringBuilder();
+
+            returnSB.AppendLine("economy_event_queue : " + _nameless + " {");
+
+            returnSB.AppendLine(" data: " + data.Count);
+            for (int i = 0; i < data.Count; i++)
+                returnSB.AppendLine(" data[" + i + "]: " + data[i]);
+
+            returnSB.AppendLine("}");
+
+            returnString = returnSB.ToString();
+
+            return returnString;
+        }
+    }
+}
