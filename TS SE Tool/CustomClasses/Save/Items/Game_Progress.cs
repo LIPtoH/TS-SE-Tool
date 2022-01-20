cd6e95d23e3eb2386a2b717a82283fe35e4d71a8
Gaev Kirill <liptoh890@gmail.com>
2022-01-20 14:37:33 +0500

    Save Game progress structure [add]

---
+++ TS SE Tool/CustomClasses/Save/Items/Game_Progress.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TS_SE_Tool.Save.DataFormat;
+
+namespace TS_SE_Tool.Save.Items
+{
+    class Game_Progress
+    {
+        internal string generic_transports { get; set; } = "";
+        internal string undamaged_transports { get; set; } = "";
+        internal string clean_transports { get; set; } = "";
+
+        internal List<SCS_String> owned_trucks { get; set; } = new List<SCS_String>();
+
+        internal Game_Progress()
+        { }
+
+        internal Game_Progress(string[] _input)
+        {
+            string tagLine = "", dataLine = "";
+
+            foreach (string currentLine in _input)
+            {
+                if (currentLine.Contains(':'))
+                {
+                    string[] splittedLine = currentLine.Split(new char[] { ':' }, 2);
+
+                    tagLine = splittedLine[0].Trim();
+                    dataLine = splittedLine[1].Trim();
+                }
+                else
+                {
+                    tagLine = currentLine.Trim();
+                    dataLine = "";
+                }
+
+                switch (tagLine)
+                {
+                    case "":
+                        {
+                            break;
+                        }
+
+                    case "generic_transports":
+                        {
+                            generic_transports = dataLine;
+                            break;
+                        }
+
+                    case "undamaged_transports":
+                        {
+                            undamaged_transports = dataLine;
+                            break;
+                        }
+
+                    case "clean_transports":
+                        {
+                            clean_transports = dataLine;
+                            break;
+                        }
+
+                    case "owned_trucks":
+                        {
+                            owned_trucks.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("owned_trucks["):
+                        {
+                            owned_trucks.Add(dataLine);
+                            break;
+                        }
+
+                }
+            }
+        }
+
+        internal string PrintOut(uint _version, string _nameless)
+        {
+            string returnString = "";
+
+            StringBuilder returnSB = new StringBuilder();
+
+            returnSB.AppendLine("game_progress : " + _nameless + " {");
+
+            returnSB.AppendLine(" generic_transports: " + generic_transports);
+            returnSB.AppendLine(" undamaged_transports: " + undamaged_transports);
+            returnSB.AppendLine(" clean_transports: " + clean_transports);
+
+            returnSB.AppendLine(" owned_trucks: " + owned_trucks.Count);
+            for (int i = 0; i < owned_trucks.Count; i++)
+                returnSB.AppendLine(" owned_trucks[" + i + "]: " + owned_trucks[i]);
+
+            returnSB.AppendLine("}");
+
+            returnString = returnSB.ToString();
+
+            return returnString;
+        }
+    }
+}
---
+++ TS SE Tool/CustomClasses/Save/Items/Transport_Data.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TS_SE_Tool.Save.DataFormat;
+
+namespace TS_SE_Tool.Save.Items
+{
+    class Transport_Data
+    {
+        internal int distance { get; set; } = 0;
+        internal int time { get; set; } = 0;
+        internal int money { get; set; } = 0;
+
+        internal List<int> count_per_adr { get; set; } = new List<int>();
+
+        internal List<SCS_String> docks { get; set; } = new List<SCS_String>();
+
+        internal List<int> count_per_dock { get; set; } = new List<int>();
+
+        internal Transport_Data()
+        { }
+
+        internal Transport_Data(string[] _input)
+        {
+            string tagLine = "", dataLine = "";
+
+            foreach (string currentLine in _input)
+            {
+                if (currentLine.Contains(':'))
+                {
+                    string[] splittedLine = currentLine.Split(new char[] { ':' }, 2);
+
+                    tagLine = splittedLine[0].Trim();
+                    dataLine = splittedLine[1].Trim();
+                }
+                else
+                {
+                    tagLine = currentLine.Trim();
+                    dataLine = "";
+                }
+
+                switch (tagLine)
+                {
+                    case "":
+                        {
+                            break;
+                        }
+
+                    case "distance":
+                        {
+                            distance = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case "time":
+                        {
+                            time = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case "money":
+                        {
+                            money = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case "count_per_adr":
+                        {
+                            count_per_adr.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("count_per_adr["):
+                        {
+                            count_per_adr.Add(int.Parse(dataLine));
+                            break;
+                        }
+
+                    case "docks":
+                        {
+                            docks.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("docks["):
+                        {
+                            docks.Add(dataLine);
+                            break;
+                        }
+
+                    case "count_per_dock":
+                        {
+                            count_per_dock.Capacity = int.Parse(dataLine);
+                            break;
+                        }
+
+                    case var s when s.StartsWith("count_per_dock["):
+                        {
+                            count_per_dock.Add(int.Parse(dataLine));
+                            break;
+                        }
+
+                }
+            }
+        }
+
+        internal string PrintOut(uint _version, string _nameless)
+        {
+            string returnString = "";
+
+            StringBuilder returnSB = new StringBuilder();
+
+            returnSB.AppendLine("transport_data : " + _nameless + " {");
+
+            returnSB.AppendLine(" distance: " + distance.ToString());
+            returnSB.AppendLine(" time: " + time.ToString());
+            returnSB.AppendLine(" money: " + money.ToString());
+
+            returnSB.AppendLine(" count_per_adr: " + count_per_adr.Count);
+            for (int i = 0; i < count_per_adr.Count; i++)
+                returnSB.AppendLine(" count_per_adr[" + i + "]: " + count_per_adr[i].ToString());
+
+            returnSB.AppendLine(" docks: " + docks.Count);
+            for (int i = 0; i < docks.Count; i++)
+                returnSB.AppendLine(" docks[" + i + "]: " + docks[i].ToString());
+
+            returnSB.AppendLine(" count_per_dock: " + count_per_dock.Count);
+            for (int i = 0; i < count_per_dock.Count; i++)
+                returnSB.AppendLine(" count_per_dock[" + i + "]: " + count_per_dock[i].ToString());
+
+            returnSB.AppendLine("}");
+
+            returnString = returnSB.ToString();
+
+            return returnString;
+        }
+    }
+}
