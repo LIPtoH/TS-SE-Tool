5500e793d01d84c148bdf3cefa932cd4b5031fcc
Gaev Kirill <liptoh890@gmail.com>
2022-03-23 14:41:58 +0500

    System Freight market Better route distance search [update]

--- TS SE Tool/CustomClasses/ExternalData/Routes.cs
+++ TS SE Tool/CustomClasses/ExternalData/Routes.cs
@@ -32,34 +32,133 @@ namespace TS_SE_Tool
 
         public string[] GetRouteData(string _starting_city, string _starting_company, string _destination_city, string _destination_company)
         {
+            string[] CompanyToCompanyTrueRoute = RoutesList.Find(x => x[0] == _starting_city && x[1] == _starting_company && x[2] == _destination_city && x[3] == _destination_company);
 
-            string[] strArray = RoutesList.Find(x => x[0] == _starting_city && x[1] == _starting_company && x[2] == _destination_city && x[3] == _destination_company);
-
-            if (strArray == null)
+            if (CompanyToCompanyTrueRoute != null)
             {
-                List<string[]> strArray2 = RoutesList.FindAll(x => x[0] == _starting_city && x[2] == _destination_city || x[0] == _destination_city && x[2] == _starting_city);
+                return CompanyToCompanyTrueRoute;
+            }
+            else
+            {
+                // Same City Company to Company
+                if (_starting_city == _destination_city)
+                    return new string[] { _starting_city, _starting_company, _destination_city, _destination_company, "1", "0", "0" };
 
-                if (strArray2.Count == 0)
+                // Searching variants
+
+                // City to City both directions
+                List<string[]> CityToCityRouteBiDirectional = RoutesList.FindAll(x => x[0] == _starting_city && x[2] == _destination_city || x[0] == _destination_city && x[2] == _starting_city);
+
+                if (CityToCityRouteBiDirectional.Count > 0)
                 {
-                    if(_starting_city == _destination_city)
-                        return new string[] { _starting_city, _starting_company, _destination_city, _destination_company, "1", "0", "0" };
-                    else
-                        return new string[] { _starting_city, _starting_company, _destination_city, _destination_company, "11111", "0", "0" };
+                    CityToCityRouteBiDirectional = CityToCityRouteBiDirectional.OrderBy(x => x[4]).ToList();
+
+                    string[] CityToCityRouteBiDirectionalShortest = CityToCityRouteBiDirectional.First();
+
+                    return CityToCityRouteBiDirectionalShortest;                    
                 }
                 else
                 {
-                    strArray2 = strArray2.OrderBy(x => x[4]).ToList();
-                    strArray = strArray2.First();
+                    // Search for transit routes
+                    List<string[]> FiltereRoutes = new List<string[]>();
 
-                    if (strArray != null)
-                        return strArray;
+                    //---
+                    List<string[]> CityToCityRouteSingleEnded1 = RoutesList.FindAll(x => x[0] == _starting_city);
+
+                    List<string> CityList1 = GetList(CityToCityRouteSingleEnded1, 2);
+                    CityList1.Sort();
+                    //---
+                    List<string[]> CityToCityRouteSingleEnded2 = RoutesList.FindAll(x => x[2] == _destination_city);
+
+                    List<string> CityList2 = GetList(CityToCityRouteSingleEnded2, 0);
+                    CityList2.Sort();
+                    //---
+                    List<string[]> CityToCityRouteSingleEnded3 = RoutesList.FindAll(x => x[0] == _destination_city);
+
+                    List<string> CityList3 = GetList(CityToCityRouteSingleEnded3, 2);
+                    CityList3.Sort();
+                    //---
+                    List<string[]> CityToCityRouteSingleEnded4 = RoutesList.FindAll(x => x[2] == _starting_city);
+
+                    List<string> CityList4 = GetList(CityToCityRouteSingleEnded4, 0);
+                    CityList4.Sort();
+                    //---
+
+                    // Get cities
+                    var startToEndTransitCities = CityList1.Intersect(CityList2).ToList();
+                    var endToStartTransitCities = CityList3.Intersect(CityList4).ToList();
+
+                    List<string[]> SearchResults = new List<string[]>();
+
+                    // Get routes
+                    if (startToEndTransitCities.Count > 0)
+                    {
+                        foreach (string entry in startToEndTransitCities)
+                        {
+                            var entryRoute = GetRouteWithTransit(_starting_city, _destination_city, entry);
+                            if (entryRoute != null)
+                                SearchResults.Add(entryRoute);
+                        }
+                    }
+
+                    if (endToStartTransitCities.Count > 0)
+                    {
+                        foreach (string entry in endToStartTransitCities)
+                        {
+                            var entryRoute = GetRouteWithTransit(_destination_city, _starting_city, entry);
+                            if (entryRoute != null)
+                                SearchResults.Add(entryRoute);
+                        }
+                    }
+
+                    // Filter routes
+                    SearchResults = SearchResults.OrderBy(x => x[4]).ToList();
+
+                    if (SearchResults.Count > 0)
+                        return SearchResults.First();
                     else
                         return new string[] { _starting_city, _starting_company, _destination_city, _destination_company, "11111", "0", "0" };
-                }
 
-            }                
-            else
-                return strArray;
+                    //===
+                    List<string> GetList (List<string[]> _input, byte idx)
+                    {
+                        List<string> Data = new List<string>();
+
+                        foreach (string[] entry in _input)
+                        {
+                            Data.Add(entry[idx]);
+                        }
+
+                        Data = Data.Distinct().ToList();
+
+                        return Data;
+                    }
+
+                    //===
+                    string[] GetRouteWithTransit(string _start, string _end, string _transit)
+                    {
+                        List<string[]> startToTransit = RoutesList.FindAll(x => x[0] == _start && x[2] == _transit);
+
+                        if (startToTransit.Count > 0)
+                        {
+                            List<string[]> transitToEnd = RoutesList.FindAll(x => x[0] == _transit && x[2] == _end);
+
+                            string[] shortestSTT = startToTransit.OrderBy(x => x[4]).ToList().First();
+                            string[] shartestTTE = transitToEnd.OrderBy(x => x[4]).ToList().First();
+
+                            return new string[] { _starting_city, _starting_company, _destination_city, _destination_company,
+                                                (int.Parse(shortestSTT[4]) + int.Parse(shartestTTE[4])).ToString(),
+                                                (int.Parse(shortestSTT[5]) + int.Parse(shartestTTE[5])).ToString(),
+                                                (int.Parse(shortestSTT[6]) + int.Parse(shartestTTE[6])).ToString()
+                                                };
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }    
+            }
         }
 
         public List<string[]> GetRoutes()
