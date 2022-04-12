f2fcc33678225cdfa9705b17b6a3685a50ba9c75
Gaev Kirill <liptoh890@gmail.com>
2022-04-12 15:05:16 +0500

    Save Block core Unidentified lines & Error Message write [update]

--- TS SE Tool/CustomClasses/Save/Items/SiiNBlockCore.cs
+++ TS SE Tool/CustomClasses/Save/Items/SiiNBlockCore.cs
@@ -8,10 +8,21 @@ namespace TS_SE_Tool.Save.Items
 {
     internal class SiiNBlockCore
     {
+        internal List<string> UnidentifiedLines = new List<string>();
 
         internal void removeWritenBlock(string _input)
         {
             FormMain.SiiNunitData.NamelessControlList.Remove(_input);
+        }
+
+        internal string WriteUnidentifiedLines()
+        {
+            return String.Join(Environment.NewLine, UnidentifiedLines);
+        }
+
+        internal string WriteErrorMsg(string _message, string _tagLine, string _dataLine)
+        {
+            return _message + Environment.NewLine + this.GetType().Name.ToLower() + " | " + _tagLine + " = " + _dataLine;
         }
     }
 }
