6cfab7fe96c94c59afdfd51c0ed13f5f538f5833
LIPtoH <liptoh890@gmail.com>
2023-05-07 21:15:03 +0500

    v0.3.9.0

--- TS SE Tool/Properties/AssemblyInfo.cs
+++ TS SE Tool/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TS SE Tool")]
-[assembly: AssemblyCopyright("Copyright © 2016 - 2022")]
+[assembly: AssemblyCopyright("Copyright © 2016 - 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.8.1")]
-[assembly: AssemblyFileVersion("0.3.8.1")]
+[assembly: AssemblyVersion("0.3.9.0")]
+[assembly: AssemblyFileVersion("0.3.9.0")]
